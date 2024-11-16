6e9274080ce72547c23bc96c205936f723094b29
Tóth István <toth.istvan@danubiusinfo.hu>
2024-11-16 20:48:15 +0100

    mod 101

--- models/staging/stg_stores.sql
+++ models/staging/stg_stores.sql
@@ -1,6 +1,6 @@
 with source as (
 
-    select * from {{ source('jaffle_shop', 'br_store', include_database = False) }}
+    select * from {{ source('jaffle_shop', 'v_lh_br_store', include_database = False) }}
 
 ),
 
@@ -8,8 +8,7 @@ dbt_stores as (
 
     select
         *
-    from br_store
-
+     from v_lh_br_store
 )
 
 select * from dbt_stores
